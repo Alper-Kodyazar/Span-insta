a1d066c4d580b2bc01d2c2d232614411f60a3670
Alper-Kodyazar <alperenpoyraz5034@gmail.com>
2025-04-04 00:10:54 +0300

    Update spam insta (2).py

--- spam insta (2).py
+++ spam insta (2).py
@@ -2,5 +2,5 @@ from pyautogui import write,press
 from time import sleep
 sleep(5)
 while True:
-    write("kadir o parmak sana girsin")
+    write("Emir ali reels atarsan gaysın")
     press("enter")
